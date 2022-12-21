ab1ac31243898ac5bdd3c9d0ba408bbc38fe128e
Jasleen Kaur <91721589+Jasleen8801@users.noreply.github.com>
2022-12-21 10:31:39 +0530

    Upload index.js

---
+++ src/components/Lists/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import LandHeader from "../LandingPage/LandHeader"
+import ListMain from "./ListMain";
+
+const Index = () => {
+    return (
+        <div>
+            <LandHeader/>
+            <ListMain/>
+        </div>
+    )
+}
