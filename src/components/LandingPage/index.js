32285ae00819a0e2f35947df6e86e5c602418e97
Jasleen Kaur <91721589+Jasleen8801@users.noreply.github.com>
2022-12-21 10:28:19 +0530

    Upload index.js

---
+++ src/components/LandingPage/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import LandHeader from './LandHeader'
+import LandingMain from './LandingMain'
+import LandingRecommendedPost from './LandingRecommendedPost'
+
+const Index = () => {
+    return(
+        <div>
+            <LandHeader/>
+            <LandingMain/>
+            <LandingRecommendedPost/>
+        </div>
+    )
+}
+
+export default Index;
