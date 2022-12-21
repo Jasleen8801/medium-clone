69d67c5a049a255e61cb8ee8aa446122daa65812
Jasleen Kaur <91721589+Jasleen8801@users.noreply.github.com>
2022-12-21 10:32:02 +0530

    Upload ListMain.js

---
+++ src/components/Lists/ListMain.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ListMain = () => {
+    return (
+        <div className="story-main">
+            <div className="story-main-container">
+                <div className="story-main-header">
+                    <h2>Your lists</h2>
+                </div>
+                <div className="stories-content">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ListMain;
